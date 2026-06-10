e11eaa05de1d4244527d9cf94620e131bdfd9e8a
Elijah Jones <elijah.z.jones2009@gmail.com>
2026-06-11 08:45:17 +1000

    first commit

---
+++ cave.py
@@ -0,0 +1,6 @@
+class Cave:
+    def__init__(self, name, description, item, character):
+        self.name = name
+        self.description = description
+        self.item = item
+        self.character = character
